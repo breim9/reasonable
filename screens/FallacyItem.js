46ef074367fa72cc66e6d6018134458de305de4a
Benjamin Reimer <benreimer9@gmail.com>
2020-01-08 08:52:52 -0500

    fallacy item styling

--- screens/FallacyItem.js
+++ screens/FallacyItem.js
@@ -12,10 +12,12 @@ export default class FallacyItem extends Component {
     let fallacyItem = this.props.navigation.getParam('item', 'Fallacy Item');
 
     return (
-      <View style={{flex:1}}>
-        <Text>{fallacyItem.name}</Text>
-        <Text>{fallacyItem.definition}</Text>
-        <Text>{fallacyItem.example}</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>{fallacyItem.name}</Text>
+        </View>
+        <Text style={styles.definition}>{fallacyItem.definition}</Text>
+        <Text style={styles.example}>{fallacyItem.example}</Text>
       </View>
     )
   }
@@ -24,5 +26,30 @@ export default class FallacyItem extends Component {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+      flex: 1,
+      backgroundColor: "#2A2B3D",
+      padding: 20,
+  },
+  header : {
+      backgroundColor: "#2A2B3D",
+  },
+  title: {
+      fontFamily: 'roboto-mono-medium',
+      fontSize : 24,
+      color: '#fff',
+      marginTop: 20,
+      marginBottom: 30,
+  },
+  definition: {
+      color:"#fff",
+      marginBottom: 12,
+      fontSize: 18,
+      lineHeight: 26,
+  },
+  example: {
+      color:"#ADA3FF",
+      fontSize: 16,
+      lineHeight: 20,
+  }
 })
